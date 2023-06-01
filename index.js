a1074964a6e138524755874fa86bd85948514dc2
Manitkumartomar <135132264+Manitkumartomar@users.noreply.github.com>
2023-06-01 00:30:27 -0700

    My first repl......

---
+++ index.js
@@ -0,0 +1,12 @@
+let name = "Name-Manit Kumar";
+console.log(name);
+let age = "Age- 22 ";
+console.log(age);
+let favfood = "Favourite Food - Cheese";
+console.log(favfood);
+let scname = "School Name - L I C Kelanpur";
+let hobb = "Hobby-Listening Music";
+console.log(scname);
+console.log(hobb);
+let home = "Home-Bijnor";
+console.log(home);
