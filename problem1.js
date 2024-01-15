397af1303ef9fc2ae4c497cb8d67e626e1ac3d19
Manitkumartomar <manitkumar8979@gmail.com>
2024-01-15 18:54:46 +0530

    I have removed unnecessary code from here

--- problem1.js
+++ problem1.js
@@ -3,4 +3,4 @@ console.log("Manit Kumar");
 console.log(75*5);
 console.log(10**3);
 console.log(1*2*3*4*5*6);
-console.log("There are some problem with me to take this step of private sector and I can't understand these things properly,so please tell me How can I deal with these things. ");
+
